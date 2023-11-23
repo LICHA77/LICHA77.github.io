8f2c8c0ed1209f648ae8d17811b5cf0b70a8f187
UNMES <57911662+LICHA77@users.noreply.github.com>
2023-11-23 00:24:51 -0300

    Add files via upload

---
+++ vlb/vlb_engine/vlbdata1.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function(){ 
+    window.Lightbox = new jQuery().visualLightbox
+    ({borderSize:10,classNames:'vlightbox1',closeLocation:'top',descSliding:true,enableRightClick:true,enableSlideshow:false,pauseLocation:'imageContainerMain',prefix:'vlb1',resizeSpeed:7,slideTime:4,startZoom:true}) 
+    });
