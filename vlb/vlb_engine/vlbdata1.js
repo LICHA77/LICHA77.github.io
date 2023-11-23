928c49f605e5f985a7e274069eb3c4d235ad106c
UNMES <57911662+LICHA77@users.noreply.github.com>
2023-11-23 07:41:18 -0300

    Delete vlb directory

--- vlb/vlb_engine/vlbdata1.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function(){ 
-    window.Lightbox = new jQuery().visualLightbox
-    ({borderSize:10,classNames:'vlightbox1',closeLocation:'top',descSliding:true,enableRightClick:true,enableSlideshow:false,pauseLocation:'imageContainerMain',prefix:'vlb1',resizeSpeed:7,slideTime:4,startZoom:true}) 
-    });
